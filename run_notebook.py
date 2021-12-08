94280e2cc6591469cdc5ba1ed74915e89f2f644b
bninjaw <bninjaw>
2021-12-08 20:53:59 +0000

    updated
    removed python requirements

--- run_notebook.py
+++ run_notebook.py
@@ -8,12 +8,24 @@ dbutils.notebook.run("dbfs:/FileStore/bw_sandbox/pants", 60)
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC 
+# MAGIC pip install pyarrow pandas pex
+# MAGIC pex pyspark pyarrow pandas -o pyspark_pex_env.pex
+
+# COMMAND ----------
+
+ ./pyspark_pex_env.pex -c "import pandas; print(pandas.__version__)"
+1.1.5
+
+# COMMAND ----------
+
+# MAGIC %sh
 # MAGIC ./pants package spark_example/src/python/example
 
 # COMMAND ----------
 
-# MAGIC %sh ls
+export PYSPARK_DRIVER_PYTHON=python
+export PYSPARK_PYTHON=./pyspark_pex_env.pex
+pyspark --files pyspark_pex_env.pex
 
 # COMMAND ----------
 
