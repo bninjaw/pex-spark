a451dd2b415cab4801fd2fc9ed71a8b02a104dda
bninjaw <bninjaw>
2021-12-08 20:01:31 +0000

    tester

--- run_notebook.py
+++ run_notebook.py
@@ -1,9 +1,19 @@
 # Databricks notebook source
-dbutils.notebook.run("./pants", 60)
+dbutils.notebook.run("dbfs:/FileStore/bw_sandbox/pants", 60)
 
 # COMMAND ----------
 
-# MAGIC %run /pants
+# MAGIC %run ./pants
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC 
+# MAGIC ./pants package spark_example/src/python/example
+
+# COMMAND ----------
+
+# MAGIC %sh ls
 
 # COMMAND ----------
 
